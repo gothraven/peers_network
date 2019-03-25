b7b309cd151082fdfe290b40409fc223b3ad1f79
Safiy Errahmane Zaghbane <safiy-errahmane.zaghbane@etu.u-pec.fr>
2019-03-25 02:08:26 +0100

    Fixed the interface and dialogs

--- src/main/java/com/upec/peers/Interface/PeerInterface.java
+++ src/main/java/com/upec/peers/Interface/PeerInterface.java
@@ -1,8 +1,11 @@
 package com.upec.peers.Interface;
 
+import com.upec.peers.Interface.dialogs.ConnexionDialog;
+import com.upec.peers.Interface.dialogs.DownloadFile;
+import com.upec.peers.Interface.dialogs.SendMessageDialog;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class PeerInterface extends JFrame{
     private JPanel rootPanel;
@@ -10,60 +13,74 @@ public class PeerInterface extends JFrame{
     private JPanel peerListPanel;
     private JPanel connexionListPanel;
     private JPanel actionListPanel;
-    private JButton connect;
-    private JButton disconnect;
-    private JButton LISTOFFILESButton;
-    private JButton LISTOFPEERSButton;
-    private JButton DOWNLOADButton;
-    private JList listOfPeers;
-    private JList listOfActions;
+    private DefaultListModel<String> connectionsListModal;
+    private DefaultListModel<String> knownPeersListModal;
+    private JList<String> connectionsList;
+    private JList<String> knownPeersList;
+    private JButton connectButton;
+    private JButton disconnectButton;
+    private JButton listOfFilesButton;
+    private JButton listOfPeersButton;
+    private JButton downloadButton;
+    private JButton sendMessageButton;
     private JTextArea console;
-    private JButton SENDMESSAGEButton;
 
     public PeerInterface() {
+        add(rootPanel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(new Dimension(800, 400));
 
-        add(rootPanel) ;
-        connect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                var d = new ConnexionDialog() ;
-                d.show();
+        consolePanel.setPreferredSize(new Dimension(400, 600));
+        peerListPanel.setPreferredSize(new Dimension(150, 600));
+        connexionListPanel.setPreferredSize(new Dimension(150, 600));
+        actionListPanel.setSize(new Dimension(100, 600));
+
+        connectionsListModal = new DefaultListModel<>();
+        knownPeersListModal = new DefaultListModel<>();
+        connectionsList.setModel(connectionsListModal);
+        knownPeersList.setModel(knownPeersListModal);
+        connectionsListModal.addElement("test:123");
+
+        console.setBackground(Color.BLACK);
+        console.setForeground(Color.LIGHT_GRAY);
+        console.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        console.append("WECOME HOME\n");
+
+        connectButton.addActionListener(e -> {
+            var dialog = new ConnexionDialog((address, port) -> System.out.println(address + " : " + port));
+            dialog.setVisible(true);
+            this.connectionsListModal.addElement("here we go");
+        });
+
+        disconnectButton.addActionListener(e -> {
+            if (! this.connectionsList.isSelectionEmpty()) {
+                JOptionPane.showMessageDialog(null, "You were disconnected", "Disconnected", JOptionPane.INFORMATION_MESSAGE);
             }
         });
-        disconnect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
 
-
-               new JOptionPane().showMessageDialog(null, "DISCONNECt", "Information", JOptionPane.INFORMATION_MESSAGE);
+        listOfFilesButton.addActionListener(e -> {
+            if (! this.connectionsList.isSelectionEmpty()) {
+                console.append("new files\n");
             }
         });
-        LISTOFFILESButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new JOptionPane().showMessageDialog(null, "List Of Files", "Information", JOptionPane.INFORMATION_MESSAGE);
 
+        listOfPeersButton.addActionListener(e -> {
+            if (! this.connectionsList.isSelectionEmpty()) {
+                knownPeersListModal.addElement("new peer");
             }
         });
-        LISTOFPEERSButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new JOptionPane().showMessageDialog(null, "List Of Peers ", "Information", JOptionPane.INFORMATION_MESSAGE);
 
+        downloadButton.addActionListener(e -> {
+            if (! this.connectionsList.isSelectionEmpty()) {
+                var dialog = new DownloadFile(fileName -> System.out.println("file: " + fileName));
+                dialog.setVisible(true);
             }
         });
-        DOWNLOADButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                var d = new DownloadFile() ;
-                d.show();
-            }
-        });
-        SENDMESSAGEButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                var d = new SendMessageDialog() ;
-                d.show();
+
+        sendMessageButton.addActionListener(e -> {
+            if (! this.connectionsList.isSelectionEmpty()) {
+                var d = new SendMessageDialog(s -> System.out.println("message: " + s));
+                d.setVisible(true);
             }
         });
     }
--- src/main/java/com/upec/peers/Main.java
+++ src/main/java/com/upec/peers/Main.java
@@ -1,14 +1,11 @@
 package com.upec.peers;
 
 
-import com.upec.peers.Interface.PeerInterface;
-
 public class Main {
 
 	public static void main(String[] args) {
-//lancer l'interface
-			PeerInterface pi = new PeerInterface();
-			pi.setVisible(true );
-
+		//lancer l'interface
+//		PeerInterface pi = new PeerInterface();
+//		pi.setVisible(true);
 	}
 }
