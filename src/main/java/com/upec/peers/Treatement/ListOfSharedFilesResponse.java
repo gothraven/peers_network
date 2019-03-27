7eb1ccfcb27257d3140d05a5cc19a61d1482f8db
Nadir Si Mohammed <nadir.si-mohammed@etu.u-pec.fr>
2019-03-27 21:33:46 +0100

    update

---
+++ src/main/java/com/upec/peers/Treatement/ListOfSharedFilesResponse.java
@@ -0,0 +1,44 @@
+package com.upec.peers.Treatement;
+
+import com.upec.peers.Server.SerializerBuffer;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfSharedFilesResponse implements Serializable {
+
+    public static byte ID = 0x06;
+    public Creator<ListOfSharedFilesResponse> creator = serializerBuffer -> {
+        int listLength = serializerBuffer.readInt();
+        List<SharedFile> listOfFiles = new ArrayList<>();
+        for (int i = 0; i < listLength; i++) {
+            var name = serializerBuffer.readString();
+            var size = serializerBuffer.readLong();
+            listOfFiles.add(new SharedFile(name, size));
+        }
+        return new ListOfSharedFilesResponse(listOfFiles);
+    };
+    private List<SharedFile> listOfSharedFiles;
+
+    public ListOfSharedFilesResponse(List<SharedFile> listOfSharedFiles) {
+        this.listOfSharedFiles = listOfSharedFiles;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+    @Override
+    public SerializerBuffer serialize() {
+        SerializerBuffer serializerBuffer = new SerializerBuffer(ByteBuffer.allocate(512));
+        serializerBuffer.writeByte(ID);
+        serializerBuffer.writeInt(listOfSharedFiles.size());
+        for (SharedFile listOfSharedFile : listOfSharedFiles) {
+            serializerBuffer.writeString(listOfSharedFile.getName());
+            serializerBuffer.writeLong(listOfSharedFile.getSize());
+        }
+        return serializerBuffer;
+    }
+}
